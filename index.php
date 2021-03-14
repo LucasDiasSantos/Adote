abe6abf193d7f39302c39b80bf4841670ec450cd
LucasDiasSantos <lucas_dias_santos@hotmail.com>
2021-03-14 14:11:49 -0300

    Update index.php
    Corrigindo o arquivo do site, a estrutura estava errada.

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
-<html>
-    <Head>
-        <meta>
-        <Title>BuscaPet</Title>
-    </Head>
-    <body>
-        <?php 
-            echo '<p></p>'     
-               
-        ?>
-    </body>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BuscaPet</title>
+</head>
+<body>
+    <?php 
+            
+    
+    
+    ?>   
+</body>
 </html>
