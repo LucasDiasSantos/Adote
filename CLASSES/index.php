2f3be32b2a834e0f9f3a729e40095a1e3e08df86
Bruno Sabec <80073673+BrunoSabec@users.noreply.github.com>
2021-04-22 17:20:21 -0300

    Add files via upload

---
+++ CLASSES/index.php
@@ -0,0 +1,28 @@
+<html Lang ="pt-br">
+<head>
+    <meta charset="utf-8"/>
+    <title>Tela de Login</title>
+    <link rel="stylesheet" href="CSS/estilo.css">
+
+</head>
+<body>
+    <div id ="corpo-form">
+    <h1>Entrar</h1>
+        <form method="POST">
+        <input type="email" placeholder="Usuário" name = "email">
+        <input type="password" placeholder="Senha" name= "senha">
+        <input type="submit" value="ACESSAR">
+        <a href="cadastrar.php"> Ainda não é inscrito?<strong>Cadastre-se</strong></a>
+        </form>
+    </div>
+
+<?php
+
+
+
+
+
+?>
+
+</body>
+</html>
